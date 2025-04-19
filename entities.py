593d8d31932a41d90b92f0a4de44a6dcaf5d3c4e
Luis Casas <luisscasas45@gmail.com>
2025-04-18 20:03:50 -0600

    FEAT/ implement contact management functionality with logging

---
+++ entities.py
@@ -0,0 +1,42 @@
+import json
+import files
+
+def file_log(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        with open("activity.log", "a") as f:
+            f.write("Contact created\n")
+        return result
+    return wrapper
+
+def console_log(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print("Contact Created")
+        return result
+    return wrapper
+
+def save_data(file_name, data):
+    content = files.read(file_name)
+    return files.update(file_name, data)
+
+class Contact:
+    def __init__(self, name, last_name, phone, email, street, Enum, Inum, col, mun, city, state, country):
+        self.name = name
+        self.last_name = last_name
+        self.phone = phone
+        self.email = email
+        self.street = street
+        self.Enum = Enum
+        self.Inum = Inum
+        self.col = col
+        self.mun = mun
+        self.city = city
+        self.state = state
+        self.country = country
+
+    @file_log
+    @console_log
+    def save(self):
+        file_name = "contacts.json"
+        return save_data(file_name, self.__dict__)
