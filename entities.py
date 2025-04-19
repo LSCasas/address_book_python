b63f26edd2414e275e4736db123c4115dac805dc
Luis Casas <luisscasas45@gmail.com>
2025-04-18 20:08:02 -0600

    DOCS/ update docstrings for logging and data saving functions

--- entities.py
+++ entities.py
@@ -2,6 +2,11 @@ import json
 import files
 
 def file_log(func):
+    """Register a log
+
+    Args:
+        func (_type_): function
+    """
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         with open("activity.log", "a") as f:
@@ -10,6 +15,11 @@ def file_log(func):
     return wrapper
 
 def console_log(func):
+    """Return a success message
+
+    Args:
+        func (_type_): function
+    """
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         print("Contact Created")
@@ -17,10 +27,21 @@ def console_log(func):
     return wrapper
 
 def save_data(file_name, data):
+    """Save the new content
+
+    Args:
+        file_name (_type_): name of the file
+        data (_type_): dictionary
+
+    Returns:
+        The updated file 
+    """
     content = files.read(file_name)
     return files.update(file_name, data)
 
 class Contact:
+    """Contact instance
+    """
     def __init__(self, name, last_name, phone, email, street, Enum, Inum, col, mun, city, state, country):
         self.name = name
         self.last_name = last_name
@@ -38,5 +59,7 @@ class Contact:
     @file_log
     @console_log
     def save(self):
+        """Save the new instance
+        """
         file_name = "contacts.json"
         return save_data(file_name, self.__dict__)
